5c05802d095eb70da71f526484cf66d678e24a86
Andrei Bocan <zmaxor@gmail.com>
2009-07-07 00:02:47 +0300

    Fix clicking on voting links

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,34 @@
 $(function() {
   $('span.date').prettyDate();
   $('ul.twit li p .content, ul.twits li p .content').simpleLinks();
+
+  $('.actions .vote_up').click(function() {
+    var addr = $(this).attr('href');
+    var self = this;
+    $.post(addr, '_method=put&authenticity_token=' + encodeURIComponent(AUTH_TOKEN), function() { updateRating(self, 1) }, 'json');
+    return false;
+  });
+
+  $('.actions .vote_down').click(function() {
+    var addr = $(this).attr('href');
+    var self = this;
+    $.post(addr, '_method=put&authenticity_token=' + encodeURIComponent(AUTH_TOKEN), function() { updateRating(self, -1) }, 'json');
+    return false;
+  });
+
+  function updateRating(element, delta) {
+    var container = $(element).parents('li');
+    $(container).find('span.rating').each(
+      function(i,k) {
+        $(k).html(parseInt(k.innerHTML,10) + delta)
+      }
+    )
+
+    $(container).find('span.votes').each(
+      function(i,k) {
+        $(k).html(parseInt(k.innerHTML,10) + 1)
+      }
+    )
+  }
+
 })
